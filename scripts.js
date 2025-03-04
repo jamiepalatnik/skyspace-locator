7d362e2b01256b4562768d034da0d2fab5f93a2a
Jamie Palatnik <jamiechristensen9@gmail.com>
2025-03-04 13:01:03 -0500

    Add accurate coordinates for test skyspace

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([40.734132513933325, -73.98467225762525], 15);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+var marker = L.marker([40.734132513933325, -73.98467225762525]).addTo(map);
+marker.bindPopup("<b><i>Leading</i></b><br>2023<br><br>Friends Seminary").openPopup();
